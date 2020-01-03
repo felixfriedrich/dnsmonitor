18d3dabcb6d31f0c7c7e9d8eb9d47485577bc2dc
Felix Friedrich <felix@fri.edri.ch>
2020-01-03 12:49:38 +0100

    Integration tests: added mail functionality

--- cmd/integration_test/main.go
+++ cmd/integration_test/main.go
@@ -9,11 +9,13 @@ import (
 
 type flags struct {
 	VendorFlag configuration.VendorFlag
+	Mail       bool
 }
 
 func main() {
 	f := flags{}
 	flag.Var(&f.VendorFlag, "vendor", "alerting vendor, e.g. 'messagebird'.")
+	flag.BoolVar(&f.Mail, "mail", false, "Test mail alerting")
 	flag.Parse()
 
 	fmt.Println("Using vendor:", f.VendorFlag.String())
@@ -21,6 +23,15 @@ func main() {
 	if f.VendorFlag.Vendor == alerting.MessageBird {
 		alertingAPI := alerting.New(alerting.MessageBird, alerting.SMS)
 		err := alertingAPI.SendSMS("Test")
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	if f.Mail {
+		mail := alerting.NewMail()
+		fmt.Println("Sending mail via", mail.Config().Host)
+		err := mail.Send("Test")
 		if err != nil {
 			panic(err)
 		}
