7aa768c49cb94256014f1660055352e8d9ef247d
Felix Friedrich <felix@fri.edri.ch>
2020-03-06 14:27:22 +0100

    typo

--- pkg/alerting/alerting.go
+++ pkg/alerting/alerting.go
@@ -9,7 +9,7 @@ import (
 
 //go:generate go run github.com/maxbrunsfeld/counterfeiter/v6 -generate
 
-// API abtracts the upstream library away, mainly for mocking
+// API abstracts the upstream library away, mainly for mocking
 //counterfeiter:generate . API
 type API interface {
 	SendSMS(text string) error
