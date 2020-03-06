90c7a338a182a702f65b39e59cc5a31594ee6fa2
Felix Friedrich <felix@fri.edri.ch>
2020-03-06 20:51:24 +0100

    Corrected test names

--- pkg/configuration/helper_test.go
+++ pkg/configuration/helper_test.go
@@ -5,18 +5,22 @@ import (
 	"testing"
 )
 
-func TestOptionalWithString(t *testing.T) {
+func TestMergeWithString(t *testing.T) {
 	assert.Equal(t, "Hello", merge("Hello", "World"))
 	assert.Equal(t, "World", merge("", "World"))
 }
 
-func TestOptionalWithInt(t *testing.T) {
+func TestMergeWithInt(t *testing.T) {
 	assert.Equal(t, 1, merge(0, 1))
 	assert.Equal(t, 1, merge(1, 0))
 }
 
-func TestOptionalWithBool(t *testing.T) {
+func TestMergeWithBool(t *testing.T) {
 	assert.True(t, merge(false, true).(bool))
 	assert.True(t, merge(true, false).(bool))
 }
 
+func TestMergeWithDomainList(t *testing.T) {
+	assert.Equal(t, Domains{"www.google.com"}, merge(Domains{"www.google.com"}, Domains{}).(Domains))
+	assert.Equal(t, Domains{"www.google.com"}, merge(Domains{}, Domains{"www.google.com"}).(Domains))
+}
