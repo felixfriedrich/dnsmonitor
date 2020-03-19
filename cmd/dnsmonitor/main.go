17f9a03086f3b1ba2c0aa5cebe32b7077ffbdafc
Felix Friedrich <felix.friedrich@springer.com>
2020-03-19 15:29:24 +0100

    Bumper version to 0.5

--- cmd/dnsmonitor/main.go
+++ cmd/dnsmonitor/main.go
@@ -12,7 +12,7 @@ import (
 )
 
 const (
-	version                         = "0.4.2"
+	version                         = "0.5"
 	okExitCode                      = 0
 	fileDoesntExistExitCode         = 1
 	wrongCombinationOfFlagsExitCode = 2
